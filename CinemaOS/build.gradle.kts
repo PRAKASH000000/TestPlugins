56a353354469922f73d83c9b8a8f5b494367e8b9
PRAKASH000000 <126885477+PRAKASH000000@users.noreply.github.com>
2026-08-08 09:52:13 +0530

    Configure KotlinCompile to skip metadata version check
    Add freeCompilerArgs to KotlinCompile tasks.

--- CinemaOS/build.gradle.kts
+++ CinemaOS/build.gradle.kts
@@ -3,3 +3,10 @@ extra["pkgName"] = "com.cinemaos.tv"
 extra["extVersionCode"] = 1
 
 apply(from = "$rootDir/build.gradle.kts")
+
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs += listOf("-Xskip-metadata-version-check")
+    }
+}
